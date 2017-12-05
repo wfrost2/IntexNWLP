77f31eea12c68fa3496d7713f11d4d169daebae9
Wyatt Frost <wlf@byu.edu>
2017-12-05 12:29:17 -0700

    add DAL and DB context

---
+++ IntexNWLP/DAL/NWLPContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using IntexNWLP.Models;
+
+namespace IntexNWLP.DAL
+{
+    public class NWLPContext : DbContext
+    {
+        public NWLPContext() : base("NWLPContext")
+        {
+
+        }
+
+        public DbSet<Assay_Orders> Assay_Orders { get; set; }
+        public DbSet<Assay_Results> Assay_Results { get; set; }
+        public DbSet<Assay_Tests> Assay_Tests { get; set; }
+        public DbSet<Assays> Assays { get; set; }
+        public DbSet<Catalog_Subscriptions> Catalog_Subscriptions { get; set; }
+        public DbSet<Catalog_Type> Catalog_Type { get; set; }
+        public DbSet<Compound_Samples> Compund_Samples { get; set; }
+        public DbSet<Compounds> Compounds { get; set; }
+        public DbSet<Customer_Users> Customer_Users { get; set; }
+        public DbSet<Customers> Customers { get; set; }
+        public DbSet<Employee_Users> Employee_Users { get; set; }
+        public DbSet<Employees> Employees { get; set; }
+        public DbSet<Materials> Materials { get; set; }
+        public DbSet<Materials_Tests> Materials_Tests { get; set; }
+        public DbSet<Offices> Offices { get; set; }
+        public DbSet<Order_Invoice> Order_Invoice { get; set; }
+        public DbSet<Order_Status> Order_Status { get; set; }
+        public DbSet<Orders> Orders { get; set; }
+        public DbSet<Roles> Roles { get; set; }
+        public DbSet<Sales> Sales { get; set; }
+        public DbSet<Test_Results> Test_Results { get; set; }
+        public DbSet<Tests> Tests { get; set; }
+        public DbSet<TestTube> TestTube { get; set; }
+
+    }
+}
